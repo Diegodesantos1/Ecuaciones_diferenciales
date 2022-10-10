093ad65f2af5b4b827045745b9c3c2dddd5b0a8f
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-11 01:39:21 +0200

    quitadas las gráficas, y fixeado parte del código

--- Ecuaciones_diferenciales/ecuaciones.py
+++ Ecuaciones_diferenciales/ecuaciones.py
@@ -5,38 +5,22 @@ import math
 class Ecuaciones:
     def ecuacion1():
         x = sp.Symbol("x") ; y = sp.Function("y")
-        ecuacion = sp.Eq(y(x).diff(x), (y * (x**2) - y)/(y + 1))
+        ecuacion = sp.Eq(y(x), (x**2 - 1)/(1 + 1/y(x)))
         ics = {y(3): -1}
-        print(f" La solución de la primera ecuación es {sp.dsolve(ecuacion, y(t), ics=ics)}")
-        t = np.linspace(0, 4, 100) ; y = 1/3*t**3 + 1/3*t**2
-        plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
-        plt.title("1a Ecuación Diferencial", fontsize=20, color="blue")
-        plt.grid() ; plt.show()
+        print(f" La solución de la primera ecuación es {sp.dsolve(ecuacion, y(x), ics=ics)}")
     def ecuacion2():
         t = sp.Symbol("t") ; y = sp.Function("y")
         ecuacion = sp.Eq(y(t).diff(t) * sp.sin(t), y(t) * sp.log(y(t)))
         ics = {y((math.pi/2)): math.e}
         print(f" La solución de la segunda ecuación es {sp.dsolve(ecuacion, y(t), ics=ics)}")
-        t = np.linspace(0, 2, 100) ; y = 1/3*t**3 + 1/3*t**2
-        plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
-        plt.title("2a Ecuación Diferencial", fontsize=20, color="green")
-        plt.grid() ; plt.show()
     def ecuacion3():
         t = sp.Symbol("t") ; y = sp.Function("y")
         ecuacion = sp.Eq(y(t).diff(t) - (y(t)/(t-2)), 2*(t-2)**2)
         print(f" La solución de la tercera ecuación es {sp.dsolve(ecuacion, y(t))}")
-        t = np.linspace(0, 4, 100) ; y = 1/3*t**3 + 1/3*t**2
-        plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
-        plt.title("3a Ecuación Diferencial", fontsize=10, color="red")
-        plt.grid() ; plt.show()
     def ecuacion4():
         t = sp.Symbol("t") ; y = sp.Function("y")
         ecuacion = sp.Eq(2*t*y(t).diff(t) - y(t), 3*t**2)
         print(f" La solución de la cuarta ecuación es {sp.dsolve(ecuacion, y(t))}")
-        t = np.linspace(0, 2, 100) ; y = 1/3*t**3 + 1/3*t**2
-        plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
-        plt.title("4a Ecuación Diferencial", fontsize=10, color="purple")
-        plt.grid() ; plt.show()
 
 Ecuaciones.ecuacion1()
 Ecuaciones.ecuacion2()
