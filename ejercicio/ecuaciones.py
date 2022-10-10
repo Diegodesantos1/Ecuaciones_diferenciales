142e71273ed4ff5a9a126d9e4b1f3517e3e21447
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-10 22:36:53 +0200

    creo las variables con sympy

--- ejercicio/ecuaciones.py
+++ ejercicio/ecuaciones.py
@@ -1,3 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
+
+class Ecuaciones_diferenciales:
+    def ecuacion4():
+        t = sp.Symbol('t') ; y = sp.Function('y')
+
+
+Ecuaciones_diferenciales.ecuacion1()
