737953a2d36c9ac2da19fa8b3b379b0923384b7a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-11 01:20:22 +0200

    creada y resuelta la 2a ecuación diferencial importando math para pi y e

--- ejercicio/ecuaciones.py
+++ ejercicio/ecuaciones.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
+import math
 
 class Ecuaciones_diferenciales:
     def ecuacion1():
         pass
     def ecuacion2():
-        pass
+        #  Halla la solución particular de la ecuación y' senx= y Ln y que satisfaga la condición inicial y(pi/2) = e
+        t = sp.Symbol('t') ; y = sp.Function('y')
+        ecuacion = sp.Eq(y(t).diff(t) * sp.sin(t), y(t) * sp.log(y(t)))
+        ics = {y((math.pi/2)): math.e}
+        print(f" La solución de la segunda ecuación es {sp.dsolve(ecuacion, y(t), ics=ics)}")
+        t = np.linspace(0, 2, 100) ; y = 1/3*t**3 + 1/3*t**2
+        plt.plot(t, y) ; plt.xlabel('t') ; plt.ylabel('y')
+        plt.title('2a Ecuación Diferencial', fontsize=10, color='blue')
+        plt.grid() ; plt.show()
     def ecuacion3():
         t = sp.Symbol("t") ; y = sp.Function("y")
         ecuacion = sp.Eq(y(t).diff(t) - (y(t)/(t-2)), 2*(t-2)**2)
         print(f" La solución de la tercera ecuación es {sp.dsolve(ecuacion, y(t))}")
         t = np.linspace(0, 4, 100) ; y = 1/3*t**3 + 1/3*t**2
         plt.plot(t, y) ; plt.xlabel('t') ; plt.ylabel('y')
-        plt.title('Ecuación diferencial y\' - (y/(t-2)) = 2(t-2)**2', fontsize=10, color='blue')
+        plt.title('3a Ecuación Diferencial', fontsize=10, color='blue')
         plt.grid() ; plt.show()
     def ecuacion4():
         t = sp.Symbol('t') ; y = sp.Function('y')
@@ -21,8 +30,9 @@ class Ecuaciones_diferenciales:
         print(f" La solución de la cuarta ecuación es {sp.dsolve(ecuacion, y(t))}")
         t = np.linspace(0, 2, 100) ; y = 1/3*t**3 + 1/3*t**2
         plt.plot(t, y) ; plt.xlabel('t') ; plt.ylabel('y')
-        plt.title('Ecuación diferencial 2ty\'- y = 3t**2 : FAMILIA DE SOLUCIONES', fontsize=10, color='blue')
+        plt.title('4a Ecuación Diferencial', fontsize=10, color='blue')
         plt.grid() ; plt.show()
 
+Ecuaciones_diferenciales.ecuacion2()
 Ecuaciones_diferenciales.ecuacion3()
 Ecuaciones_diferenciales.ecuacion4()
