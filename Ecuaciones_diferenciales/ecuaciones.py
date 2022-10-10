2e821c625eeb012c81f958eb2073ee76c31f0beb
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-11 01:29:44 +0200

    acabada la primera ecuación

--- Ecuaciones_diferenciales/ecuaciones.py
+++ Ecuaciones_diferenciales/ecuaciones.py
@@ -3,6 +3,15 @@ import numpy as np
 import sympy as sp
 import math
 class Ecuaciones:
+    def ecuacion1():
+        x = sp.Symbol("x") ; y = sp.Function("y")
+        ecuacion = sp.Eq(y(x).diff(x), ((x**2)*y - y)/(y + 1))
+        ics = {y(3): -1}
+        print(f" La solución de la primera ecuación es {sp.dsolve(ecuacion, y(t), ics=ics)}")
+        t = np.linspace(0, 4, 100) ; y = 1/3*t**3 + 1/3*t**2
+        plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
+        plt.title("1a Ecuación Diferencial", fontsize=10, color="blue")
+        plt.grid() ; plt.show()
     def ecuacion2():
         t = sp.Symbol("t") ; y = sp.Function("y")
         ecuacion = sp.Eq(y(t).diff(t) * sp.sin(t), y(t) * sp.log(y(t)))
@@ -10,7 +19,7 @@ class Ecuaciones:
         print(f" La solución de la segunda ecuación es {sp.dsolve(ecuacion, y(t), ics=ics)}")
         t = np.linspace(0, 2, 100) ; y = 1/3*t**3 + 1/3*t**2
         plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
-        plt.title("2a Ecuación Diferencial", fontsize=10, color="blue")
+        plt.title("2a Ecuación Diferencial", fontsize=20, color="blue")
         plt.grid() ; plt.show()
     def ecuacion3():
         t = sp.Symbol("t") ; y = sp.Function("y")
@@ -28,7 +37,7 @@ class Ecuaciones:
         plt.plot(t, y) ; plt.xlabel("t") ; plt.ylabel("y")
         plt.title("4a Ecuación Diferencial", fontsize=10, color="blue")
         plt.grid() ; plt.show()
-
+Ecuaciones.ecuacion1()
 Ecuaciones.ecuacion2()
 Ecuaciones.ecuacion3()
 Ecuaciones.ecuacion4()
