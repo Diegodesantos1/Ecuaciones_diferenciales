f4b24fe5c0162cd592a8a85f3717023bb8613509
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-10 22:37:31 +0200

    printeo la solución

--- ejercicio/ecuaciones.py
+++ ejercicio/ecuaciones.py
@@ -5,6 +5,9 @@ import sympy as sp
 class Ecuaciones_diferenciales:
     def ecuacion4():
         t = sp.Symbol('t') ; y = sp.Function('y')
+        ecuacion = sp.Eq(2*t*y(t).diff(t) - y(t), 3*t**2)
+        print(f" La solución es {sp.dsolve(ecuacion, y(t))}")
+
 
 
 Ecuaciones_diferenciales.ecuacion1()
