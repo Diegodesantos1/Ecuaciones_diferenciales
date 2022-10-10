55cf6a5ccace68bbec59495f9e18ec7ee07b5aef
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-10 22:38:34 +0200

    creo las funciones de las 4 ecuaciones

--- ejercicio/ecuaciones.py
+++ ejercicio/ecuaciones.py
@@ -3,6 +3,12 @@ import numpy as np
 import sympy as sp
 
 class Ecuaciones_diferenciales:
+    def ecuacion1():
+        pass
+    def ecuacion2():
+        pass
+    def ecuacion3():
+        pass
     def ecuacion4():
         t = sp.Symbol('t') ; y = sp.Function('y')
         ecuacion = sp.Eq(2*t*y(t).diff(t) - y(t), 3*t**2)
@@ -11,8 +17,6 @@ class Ecuaciones_diferenciales:
         plt.plot(t, y) ; plt.xlabel('t') ; plt.ylabel('y')
         plt.title('Ecuación diferencial 2ty\'- y = 3t**2')
         plt.grid() ; plt.show()
-
-
 
 
 Ecuaciones_diferenciales.ecuacion1()
