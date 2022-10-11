0ae6e866755661e94e5d270d96fa9073e8fa8d7a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-11 15:13:34 +0200

    aplicado el método pprint

--- EDO/ecuaciones.py
+++ EDO/ecuaciones.py
@@ -7,21 +7,21 @@ class Ecuaciones:
     def ecuacion1():
         x = sp.Symbol("x") ; y = sp.Symbol("y")
         e1 = integrate((1 + 1/y), y) ; e2 = integrate((x**2 - 1), x)
-        ecuacion = sp.Eq(e1, e2)
-        print(f" La solución de la primera ecuación es {sp.solve(ecuacion, y)}")
+        ecuacion = sp.Eq(e1, e2) ; sol = sp.solve(ecuacion, y)
+        print(f" La solución de la primera ecuación es {sp.pprint(sol)}")
     def ecuacion2():
         x = sp.Symbol("x") ; y = sp.Function("y")
         ecuacion = sp.Eq(y(x).diff(x) * sp.sin(x), y(x) * sp.log(y(x)))
-        ics = {y((math.pi/2)): math.e}
-        print(f" La solución de la segunda ecuación es {sp.dsolve(ecuacion, y(x), ics=ics)}")
+        ics = {y((math.pi/2)): math.e} ; sol = sp.dsolve(ecuacion, y(x), ics=ics) ;   sp.pprint(sol)
+        print(f" La solución de la segunda ecuación es: {sp.pprint(sol)}")
     def ecuacion3():
         t = sp.Symbol("t") ; y = sp.Function("y")
-        ecuacion = sp.Eq(y(t).diff(t) - (y(t)/(t-2)), 2*(t-2)**2)
-        print(f" La solución de la tercera ecuación es {sp.dsolve(ecuacion, y(t))}")
+        ecuacion = sp.Eq(y(t).diff(t) - (y(t)/(t-2)), 2*(t-2)**2) ; sol = sp.dsolve(ecuacion, y(t))
+        print(f" La solución de la tercera ecuación es {sp.pprint(sol)}")
     def ecuacion4():
         t = sp.Symbol("t") ; y = sp.Function("y")
-        ecuacion = sp.Eq(2*t*y(t).diff(t) - y(t), 3*t**2)
-        print(f" La solución de la cuarta ecuación es {sp.dsolve(ecuacion, y(t))}")
+        ecuacion = sp.Eq(2*t*y(t).diff(t) - y(t), 3*t**2) ; sol = sp.dsolve(ecuacion, y(t))
+        print(f" La solución de la cuarta ecuación es {sp.pprint(sol)}")
         t = np.linspace(0, 1, 100) ; y1 = 3*t**2/2 ; y2 = 3*t**2/2 + 3*t**3/2
         y3 = 3*t**2/2 + 3*t**3/2 + 3*t**4/2 ; y4 = 3*t**2/2 + 3*t**3/2 + 3*t**4/2 + 3*t**5/2
         plt.plot(t, y1, label="y1(t)") ; plt.plot(t, y2, label="y2(t)")
