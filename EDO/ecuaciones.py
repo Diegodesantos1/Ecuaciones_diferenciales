016fe6ca76fa8e835efb1fa7a35e0319772e460a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-13 14:43:39 +0200

    quitados los imports innecesarios

--- EDO/ecuaciones.py
+++ EDO/ecuaciones.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import sympy as sp
 from sympy import *
 import math
@@ -26,9 +24,4 @@ class Ecuaciones:
         ecuacion = sp.Eq(2*t*y(t).diff(t) - y(t), 3*t**2) ; sol = sp.dsolve(ecuacion, y(t))
         print(f" La solución de la cuarta ecuación es: \n\n\n")
         sp.pprint(sol)
-        t = np.linspace(0, 1, 100) ; y1 = 3*t**2/2 ; y2 = 3*t**2/2 + 3*t**3/2
-        y3 = 3*t**2/2 + 3*t**3/2 + 3*t**4/2 ; y4 = 3*t**2/2 + 3*t**3/2 + 3*t**4/2 + 3*t**5/2
-        plt.plot(t, y1, label="y1(t)") ; plt.plot(t, y2, label="y2(t)")
-        plt.plot(t, y3, label="y3(t)") ; plt.plot(t, y4, label="y4(t)")
-        plt.title("Familia de soluciones de la ecuación diferencial", fontsize=15, color="blue") ; plt.legend() ; plt.grid() ; plt.show()
 
